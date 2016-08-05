85f086712ca3c6fb6d23fd67793d284e4fc1b42f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-08-05 14:10:34 +0900

    add tests

--- qq_test.go
+++ qq_test.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
-var testcases = []struct {
+var testcases_readlines = []struct {
+	input  string
+	output []string
+}{
+	{
+		input: "  PID command   \n" +
+			"\n" +
+			"   1   ls       \n",
+		output: []string{
+			"  PID command   ", "   1   ls       ",
+		},
+	},
+}
+
+func TestReadLines(t *testing.T) {
+	for _, testcase := range testcases_readlines {
+		lines, err := readLines(strings.NewReader(testcase.input))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(lines, testcase.output) {
+			t.Fatalf("%q should be read as %v: got %v", testcase.input, testcase.output, lines)
+		}
+	}
+}
+
+var testcases_lines2rows = []struct {
 	input  []string
 	output [][]string
 }{
@@ -63,10 +90,19 @@ var testcases = []struct {
 			[]string{"______f1"}, {"1"},
 		},
 	},
+	{
+		input: []string{
+			"a b",
+			"1 ",
+		},
+		output: [][]string{
+			[]string{"a", "b"}, {"1", ""},
+		},
+	},
 }
 
 func TestLines2Rows(t *testing.T) {
-	for _, testcase := range testcases {
+	for _, testcase := range testcases_lines2rows {
 		rows := lines2rows(testcase.input)
 		if !reflect.DeepEqual(rows, testcase.output) {
 			t.Fatalf("%q should be parsed as %v: got %v", testcase.input, testcase.output, rows)
